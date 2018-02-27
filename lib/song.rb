90987011aef5202a330d0e1409d4854df14d0b81
Robert W <robwash@gmail.com>
2018-02-27 23:08:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     sep = artist_and_name.split(" - ")
     song_name = sep[1].split(".")
     name = sep[0]
-    artist_name = song[0]
+    artist_name = song_name[0]
 
     song = self.new
     song.name = name
